051ab7ad468b7425d9b727f15caf1b466eb03962
RushikeshPandekar <92102098+RushikeshPandekar@users.noreply.github.com>
2023-04-23 23:55:19 +0530

    Database changed from local to remote

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 
-const mongoURI="mongodb://localhost:27017/INoteBook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+// const mongoURI="mongodb://localhost:27017/INoteBook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongoURI="mongodb+srv://rushikesh22010354:Ahmednagar45@cluster0.sppuwoh.mongodb.net/test"
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
