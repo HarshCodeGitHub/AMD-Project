0950833631ca9a7cdd9a8dd4206af094641b8a75
RushikeshPandekar <92102098+RushikeshPandekar@users.noreply.github.com>
2023-05-07 18:29:47 +0530

    All Done for Version 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,12 @@ import {
 } from "react-router-dom";
 // import About from './components/About';
 import NoteState from './contexts/NoteState';
-// import Notes from './components/Notes';
+import Notes from './components/Notes';
 import Login from './components/Login';
 import Signup from './components/Signup';
-// import Alert from './components/Alert';
+import Alert from './components/Alert';
 import { useState } from 'react';
-// import Home from './components/Home';
+import Home from './components/Home';
 
 function App() {
   const [alert, setAlert] = useState(null);
@@ -30,13 +30,13 @@ function App() {
       <NoteState>
         <Router>
             <Navbar/>
-            {/* <Alert alert={alert} /> */}
+            <Alert alert={alert} />
             <Routes>
                 <Route path="/login" element={<Login giveAlert={giveAlert}/>}/>
                 <Route path="/signup" element={<Signup giveAlert={giveAlert}/>}/>
-                {/* <Route path="/" element={<Home giveAlert={giveAlert}/>}/>
+                <Route path="/" element={<Home giveAlert={giveAlert}/>}/>
                 <Route path="/yournotes" element={<Notes giveAlert={giveAlert}/>}/>
-                <Route path="/about" element={<About/>}/> */}
+                {/* <Route path="/about" element={<About/>}/> */}
               </Routes>
         </Router>
       </NoteState>
